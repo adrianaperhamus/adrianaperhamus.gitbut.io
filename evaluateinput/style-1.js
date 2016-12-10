059d51a05282705abac17365724824b7e1e32be2
Adriana Herrera Perhamus <herra719@newschool.edu>
2016-12-10 18:53:34 -0500

    Update style-1.js

--- evaluateinput/style-1.js
+++ evaluateinput/style-1.js
@@ -1,23 +1,20 @@
-$("#age-submit").click(function(){
-
-  //select the box with id of age
-  //get the value from that box
-  var ageString = $("#age").val();
-
-  //convert string to number
-  var age = parseInt(ageString);
-
-  //check if age is greater than 16
-  if ( age >= 16 ) {
-    $("#age").text("Welcome to your car.");
-    $("#car").attr("src", "https://s-media-cache-ak0.pinimg.com/736x/d7/1c/ec/d71cec6f3c59becee51942fc0da7e4f9.jpg");
-  }
-
-  else {
-    $("answer").text("Try again next year.");
-    $("#bicycle").attr("src", 
-"https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Left_side_of_Flying_Pigeon.jpg/300px-Left_side_of_Flying_Pigeon.jpg");
+$(function() {
+  //All JS goes within these brackets  
   
-});
+  $("#age-form").click(function(){
+    
+    //select the box with id of age
+    //get the value from that box
+    var ageString = $("#age").val();
 
+    //convert string to number
+    var age = parseInt(ageString);
 
+    //check if age is greater than 21
+    if ( age >= 16 ) {
+      $("#age").text("Welcome to your car.");
+    } else {
+      $("#age").text("Try again next year.");
+    }
+
+  });
